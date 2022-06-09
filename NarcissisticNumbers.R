7b417e1ee016fa19d7b45eb07122fa7631a079bf
AshleyNamini <ashley.namini@mail.utoronto.ca>
2022-06-09 17:03:30 -0400

    added check for valid entry

--- NarcissisticNumbers.R
+++ NarcissisticNumbers.R
@@ -3,3 +3,8 @@ number <- readline("Enter a positive three digit number: ")
 
 # convert user input to numeric
 number <- as.numeric(number)
+
+# check is user input is numeric
+if (is.na(number)) {
+  print("Input is invalid. Enter a positive three digit number.")
+}
